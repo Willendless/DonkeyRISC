b4493f79a7fe135330a74d96b4eb662cffbe71f9
Jiarui Li <jiaruili@berkeley.edu>
2020-04-14 11:09:43 -0700

    Modify uart_transmitter.v

--- hardware/src/io_circuits/uart_transmitter.v
+++ hardware/src/io_circuits/uart_transmitter.v
@@ -8,18 +8,18 @@ module uart_transmitter #(
     // Enqueue the to-be-sent character
     input [7:0] data_in,
     input data_in_valid,
-    output reg data_in_ready,
+    output data_in_ready,
 
     // Serial bit output
-    output reg serial_out
+    output serial_out
 );
     // See diagram in the lab guide
     localparam SYMBOL_EDGE_TIME    = CLOCK_FREQ / BAUD_RATE;
     localparam CLOCK_COUNTER_WIDTH = $clog2(SYMBOL_EDGE_TIME);
 
     (* mark_debug = "true" *) wire [9:0] tx_shift_val;
-    reg [9:0] tx_shift_next;
-    reg tx_shift_ce;
+    wire [9:0] tx_shift_next;
+    wire tx_shift_ce;
 
     // LSB to MSB
     REGISTER_CE #(.N(10)) tx_shift (
@@ -43,7 +43,7 @@ module uart_transmitter #(
 
     (* mark_debug = "true" *) wire [CLOCK_COUNTER_WIDTH-1:0] clock_counter_val;
     wire [CLOCK_COUNTER_WIDTH-1:0] clock_counter_next;
-    reg clock_counter_ce; 
+    wire clock_counter_ce; 
     wire clock_counter_rst;
 
     // Keep track of sample time and symbol edge time
@@ -56,10 +56,7 @@ module uart_transmitter #(
     );
 
     wire is_symbol_edge = (clock_counter_val == SYMBOL_EDGE_TIME - 1);
-
     wire data_in_fire = data_in_valid & data_in_ready;
-
-    // TODO: Fill in the remaining logic to implement the UART Receiver
 
     localparam IDLE = 1'b0;
     localparam WORK = 1'b1;
@@ -74,40 +71,32 @@ module uart_transmitter #(
     );
 
     always @(*) begin
+        state_reg_next = state_reg_val;
         case (state_reg_val)
-          IDLE: begin
-            data_in_ready = 1'b1;
-            clock_counter_ce = 1'b0;
-            tx_shift_ce = 1'b1;
-            tx_shift_next = 10'b0;
-            serial_out = 1'b1;
-            if (data_in_valid == 1'b1) begin
-                tx_shift_next = (data_in | (1 << 8)) << 1;
-                state_reg_next = WORK;
-            end
-            else state_reg_next = IDLE;
-          end 
-          WORK: begin
-            data_in_ready = 1'b0;
-            clock_counter_ce = 1'b1;
-            tx_shift_ce = (is_symbol_edge == 1'b1);
-            tx_shift_next = tx_shift_val >> 1;
-            serial_out = tx_shift_val[0];
-            if (bit_counter_val == 9 && is_symbol_edge == 1'b1) begin
-                state_reg_next = IDLE;
-            end
-            else state_reg_next = WORK;
-          end
+        IDLE: if (data_in_valid == 1'b1) state_reg_next = WORK;
+        WORK: if (bit_counter_val == 10) state_reg_next = IDLE; 
+        default: ; 
         endcase
     end
 
-    // bit counter
-    assign bit_counter_next     = bit_counter_val + 1; 
-    assign bit_counter_ce       = (state_reg_val == WORK) && (is_symbol_edge == 1'b1);
-    assign bit_counter_rst      = (state_reg_val == IDLE);
+    assign data_in_ready    = (state_reg_val == IDLE);
+    assign serial_out       = (state_reg_val == WORK) ? tx_shift_val[0]
+                                                        : 1'b1;
+
+    // tx shifer
+    assign tx_shift_next    = (data_in_fire == 1'b1) ? {1'b1, data_in, 1'b0}
+                                                        : tx_shift_val >> 1'b1;
+    assign tx_shift_ce      = (data_in_fire == 1'b1) || (is_symbol_edge == 1'b1);
 
     // clock counter
     assign clock_counter_next   = clock_counter_val + 1;
-    assign clock_counter_rst    = (clock_counter_next == SYMBOL_EDGE_TIME);
+    assign clock_counter_rst    = (is_symbol_edge == 1'b1);
+    assign clock_counter_ce     = (state_reg_val == WORK); 
+
+    // bit counter
+    assign bit_counter_next     = bit_counter_val + 1; 
+    assign bit_counter_ce       = (is_symbol_edge == 1'b1 && bit_counter_val < 10);
+    assign bit_counter_rst      = (state_reg_val == IDLE);
+
 
 endmodule
