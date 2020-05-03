d3fbb7dd9ae3dddf459369540fdcd2f2e571d18d
eecs151 <846053988@qq.com>
2020-05-03 00:21:59 -0700

    change make file

--- hardware/scripts/z1top.tcl
+++ hardware/scripts/z1top.tcl
@@ -1,10 +1,8 @@
 add_files -norecurse [glob src/riscv_core/*.v]
-add_files -norecurse [glob src/riscv_core/*.vh]
 add_files -norecurse [glob src/io_circuits/*.v]
 add_files -norecurse [glob src/accelerator/*.v]
-add_files -norecurse src/EECS151.v
-add_files -norecurse src/clk_wiz.v
-add_files -norecurse src/z1top.v
+add_files -norecurse [glob src/*.v]
+add_files -norecurse [glob src/*.vh]
 # Add memory initialization file
 add_files -norecurse ../software/bios151v3/bios151v3.mif
 
