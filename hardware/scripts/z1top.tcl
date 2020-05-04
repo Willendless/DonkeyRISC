ccbe966e647173a2364441c16d6003083805a6ce
Jiarui Li <jiaruili@berkeley.edu>
2020-05-04 00:08:16 -0700

    Modify conv acc

--- hardware/scripts/z1top.tcl
+++ hardware/scripts/z1top.tcl
@@ -1,17 +1,12 @@
 add_files -norecurse [glob src/riscv_core/*.v]
 add_files -norecurse [glob src/io_circuits/*.v]
 add_files -norecurse [glob src/accelerator/*.v]
-<<<<<<< HEAD
 add_files -norecurse [glob src/riscv_core/EX/*.v]
 add_files -norecurse [glob src/riscv_core/ID/*.v]
 add_files -norecurse [glob src/riscv_core/WB/*.v]
 add_files -norecurse src/EECS151.v
 add_files -norecurse src/clk_wiz.v
 add_files -norecurse src/z1top.v
-=======
-add_files -norecurse [glob src/*.v]
-add_files -norecurse [glob src/*.vh]
->>>>>>> d3fbb7dd9ae3dddf459369540fdcd2f2e571d18d
 # Add memory initialization file
 add_files -norecurse ../software/bios151v3/bios151v3.mif
 
--- hardware/src/accelerator/conv2D_pe.v
+++ hardware/src/accelerator/conv2D_pe.v
@@ -14,7 +14,7 @@ module conv2D_pe #(
     input [DWIDTH-1:0] x,
     input [DWIDTH-1:0] y,
 
-    input                   load_weights_en, // TODO
+    // input                   load_weights_en, // TODO
     input [DWIDTH-1:0]      index_i,
     input [DWIDTH-1:0]      pe_data_i,
 
@@ -30,6 +30,11 @@ module conv2D_pe #(
 
     localparam WT_SIZE      = WT_DIM * WT_DIM;
     localparam HALF_WT_DIM  = WT_DIM >> 1;
+
+    localparam STATE_IDLE       = 2'b00;
+    localparam STATE_READ_WT    = 2'b01;
+    localparam STATE_COMPUTE    = 2'b10;
+    localparam STATE_DONE       = 2'b11; 
 
     wire halo              = x == 0 | y == 0 | x == WT_DIM + 1 | y == WT_DIM + 1;
 
@@ -47,7 +52,7 @@ module conv2D_pe #(
     genvar i;
     generate
         for (i = 0; i < WT_DIM; i = i + 1) begin:in
-            assign weight_reg_en[i] = read_data_valid && load_weights_en
+            assign weight_reg_en[i] = read_data_valid && state_i == STATE_READ_WT
                                       && y == index && x == i; // TODO
             REGISTER_R_CE #(.N(DWIDTH), .INIT(32'b0)) weight_reg (
                 .q(weight_reg_q[i]),
@@ -105,9 +110,9 @@ module conv2D_pe #(
     );
 
     assign counter_d    = counter_q + 1;
-    assign counter_ce   = state_i == && counter_q < WT_DIM; // TODO
+    assign counter_ce   = state_i == STATE_COMPUTE && counter_q < WT_DIM; // TODO
 
     assign pe_data_o    = sum_val;
-    assign pe_data_valid  = ~counter_ce; 
+    assign pe_data_valid  = counter_q >= WT_DIM; 
 
 endmodule // conv_pe
