aed283e73db794e0fb35d6cd3606621ffdeeca71
li990218 <846053988@qq.com>
2020-05-11 23:36:57 -0700

    delete former file

--- README.md
+++
@@ -1,4 +0,0 @@
-# project_skeleton_sp20
-EECS 151/251A FPGA Project Skeleton for Spring 2020
-
-User: Jiarui Li & Zehua Li
--- software/151_library/string.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRING_H_
-#define STRING_H_
-
-#include "types.h"
-
-int32_t strcmp(const int8_t* s0, const int8_t* s1);
-uint32_t strlen(const int8_t* s);
-
-#endif
